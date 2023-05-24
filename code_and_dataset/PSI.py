990a3bbd2843a4027e2d3e5dcec2b3b56517936a
baize-live <1921676794@qq.com>
2023-05-24 22:57:33 +0800

    完成 PSI的实现

--- code_and_dataset/PSI.py
+++ code_and_dataset/PSI.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
-
 from loadData import *
-import csv
-import sys
-import multiprocessing
-import time
-import os
 
-def PSI(id):
-    sys.argv.extend(["--node_id", "P{}".format(id)])
-    # sys.argv.extend(["--node_id", id])
+
+def PSI(id_):
+    sys.argv.extend(["--node_id", "P{}".format(id_)])
+
     import latticex.rosetta as rtt
 
     import tensorflow as tf
@@ -27,8 +22,8 @@ def PSI(id):
     1005  32            2005  150000
     
     Tips: The loading module of this data set has already given the interface in loadData.py, just call it directly.
-    After PSI and merge the dataset, you should return a merged ciphertext table. You can reveal your ciphertext table to verify correctness.
-    After revealing, your plaintext table should be:
+    After PSI and merge the dataset, you should return a merged ciphertext table. You can reveal your ciphertext table 
+    to verify correctness. After revealing, your plaintext table should be:
     
     ID       AGE       DEPOSIT
     b'1000'  b'25'     b'50000'
@@ -40,4 +35,18 @@ def PSI(id):
         
     Code here!
     '''
-    
+
+    [rtx, rty, _] = loadData(id_)
+    # 对比结果
+    res_1 = np.array([rtx[:, 0], rtx[:, 0], rtx[:, 0], rtx[:, 0], rtx[:, 0]]).transpose()
+    res_2 = np.array([rty[:, 0], rty[:, 0], rty[:, 0], rty[:, 0], rty[:, 0]]).transpose()
+    res_comp = rtt.SecureEqual(res_1, res_2)
+    # 合并结果
+    res_merge = tf.concat([rtx, rty[:, 1:]], 1)
+    # 密文表
+    psi = rtt.SecureMul(res_merge, res_comp)
+    # 测试结果
+    # sess = tf.compat.v1.Session()
+    # sess.run(tf.compat.v1.global_variables_initializer())
+    # print(f' ps1: {sess.run(rtt.SecureReveal(psi))}')
+    return psi
--- code_and_dataset/loadData.py
+++ code_and_dataset/loadData.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
-
-import csv
 import sys
-import multiprocessing
-import time
-import os
 
-def loadData(id):
-    sys.argv.extend(["--node_id", "P{}".format(id)])
+
+def loadData(id_):
+    sys.argv.extend(["--node_id", "P{}".format(id_)])
     # sys.argv.extend(["--node_id", id])
     import latticex.rosetta as rtt
 
-    import tensorflow as tf
     import numpy as np
 
     rtt.activate("SecureNN")
@@ -19,19 +14,19 @@ def loadData(id):
     path0 = 'dataset/dataset0.csv'
     path1 = 'dataset/dataset1.csv'
 
-    header0 = ['ID', 'AGE', 'CARD']
-    header1 = ['ID', 'CREDIT', 'DEPOSIT']
+    # header0 = ['ID', 'AGE', 'CARD']
+    # header1 = ['ID', 'CREDIT', 'DEPOSIT']
 
     '''
     Step1. P0, P1 and P2 upload the dataset and generate the secret share
     '''
-    if id == 0:
-        dataset0 = np.loadtxt(open(path0), delimiter = ",", skiprows = 1)
+    if id_ == 0:
+        dataset0 = np.loadtxt(open(path0), delimiter=",", skiprows=1)
         rtx = rtt.controller.PrivateDataset(["P0"]).load_X(dataset0)
         rty = rtt.controller.PrivateDataset(["P1"]).load_X(None)
         rtz = rtt.controller.PrivateDataset(["P2"]).load_X(None)
-    elif id == 1:
-        dataset1 = np.loadtxt(open(path1), delimiter = ",", skiprows = 1)
+    elif id_ == 1:
+        dataset1 = np.loadtxt(open(path1), delimiter=",", skiprows=1)
         rtx = rtt.controller.PrivateDataset(["P0"]).load_X(None)
         rty = rtt.controller.PrivateDataset(["P1"]).load_X(dataset1)
         rtz = rtt.controller.PrivateDataset(["P2"]).load_X(None)
@@ -45,6 +40,5 @@ def loadData(id):
         rtx = rtt.controller.PrivateDataset(["P0"]).load_X(None)
         rty = rtt.controller.PrivateDataset(["P1"]).load_X(None)
         rtz = rtt.controller.PrivateDataset(["P2"]).load_X(dataset2)
-    # print(type(rtx))
-    
+
     return [rtx, rty, rtz]
